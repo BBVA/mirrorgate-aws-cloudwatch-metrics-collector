97811adb401ae5897ad9a9e2aaad14cd902172e6
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-09-25 15:52:31 +0200

    feat(AWS): take account id from mirrorgate and add copyright

--- invoker.js
+++ invoker.js
@@ -1,28 +1,37 @@
+/*
+ * Copyright 2017 Banco Bilbao Vizcaya Argentaria, S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 const config = require('./src/config.js');
 const metrics = require('./src/metrics/metrics.js');
 const template = require('./src/metrics/metricsRequestTemplate.js');
 const APICaller = require('./src/API/APICaller.js');
-
 var AWS = require('aws-sdk');
+var sts = new AWS.STS();
+var cloudWatch;
 
 AWS.config.update({region:'eu-west-1'});
 
-var sts = new AWS.STS();
-
-function assumeAWSRole(){
-
+function assumeAWSRole(accountId){
   var params = {
-    DurationSeconds: 3600, 
-    RoleArn: "arn:aws:iam::850951215438:role/test_delegated_cloudwatch_metrics_role", 
+    DurationSeconds: 900, 
+    RoleArn: "arn:aws:iam::"+accountId+":role/test_delegated_cloudwatch_metrics_role", 
     RoleSessionName: "MirrorGate"
   };
-   
-   return sts.assumeRole(params).promise();
-
+  return sts.assumeRole(params).promise();
 }
-
-var cloudWatch;
 
 function metricsCallback(err, data){
   if (err) {
@@ -56,42 +65,43 @@ function createInput(ALBName){
 }
 
 function isALBElement(listElement){
-  console.log("List element: " + listElement);
   return listElement.includes(config.collectorPrefix);
 }
 
 function cloudWatchInvoker(){
-  assumeAWSRole().then((element) => {
+  return APICaller.getAWSLoadBalancers().then((analyticsList) => {
+    var ALBList = analyticsList.filter(isALBElement);
+    ALBList.forEach( element => {
+      var cleanALB = element.trim().replace(config.collectorPrefix, '');
+      //Get account id and ALB name
+      var splitLocation = cleanALB.indexOf("/");
+      var accountId = cleanALB.substring(0,splitLocation);
+      var albName = cleanALB.substring(splitLocation+1, cleanALB.length);
 
-    cloudWatch = new AWS.CloudWatch({
-      credentials: {
-        accessKeyId: element.Credentials.AccessKeyId,
-        secretAccessKey: element.Credentials.SecretAccessKey,
-        sessionToken: element.Credentials.SessionToken,
-      },
-    });
-
-    return APICaller.getAWSLoadBalancers().then((analyticsList) => {
-        var ALBList = analyticsList.filter(isALBElement);
-        ALBList.forEach( element => {
-          var cleanALB = element.trim().replace(config.collectorPrefix, '');
-
-          Promise.all(createInput(cleanALB))
-            .then(results => {
-              APICaller.sendResultsToMirrorgate(results, cleanALB).then(result => {
+      assumeAWSRole(accountId).then((element) => {
+        cloudWatch = new AWS.CloudWatch({
+          credentials: {
+            accessKeyId: element.Credentials.AccessKeyId,
+            secretAccessKey: element.Credentials.SecretAccessKey,
+            sessionToken: element.Credentials.SessionToken,
+          }
+        });
+        Promise.all(createInput(albName))
+        .then(results => {
+          APICaller.sendResultsToMirrorgate(results, cleanALB)
+            .then(result => {
                 console.log("Elements sent to Mirrorgate");
-                console.log(result);
-              }).catch(error => {
+                console.log(result); })
+            .catch(error => {
                 console.log("POST to Mirrorgate failed!");
                 console.log(error);
-              });
-            }).catch(error => console.log(error));
-        });
+            });
+        }).catch(error => console.log(error));
+      });
     });
   }).catch((error) => {
     console.log(error);
   });
-    
 }
 
 module.exports = (() => {
