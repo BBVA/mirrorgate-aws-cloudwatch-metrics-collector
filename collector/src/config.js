80ed6ece6e9efb08cd33c42d25fca1d199974ab1
Enrique Daimiel <enrique.daimiel@bbva.com>
2017-09-27 15:25:40 +0200

    chore: allow to set collectorId configuration variable by environment variable

--- collector/src/config.js
+++ collector/src/config.js
@@ -19,5 +19,5 @@ module.exports = {
   mirrorgateGetAnalyticViewsEndpoint: process.env.MIRRORGATE_GET_ANALYTICS_ENDPOINT || 'http://localhost:8080/mirrorgate/api/user-metrics/analytic-views',
   mirrorgatePostAnalyticViewsEndpoint: process.env.MIRRORGATE_POST_ANALYTICS_ENDPOINT ||'http://localhost:8080/mirrorgate/api/user-metrics',
   collectorPrefix: 'AWS/',
-  collectorId:'mirrorgate-aws-cloudwatch-metrics-collector'
+  collectorId: process.env.COLLECTOR_ID || 'mirrorgate-aws-cloudwatch-metrics-collector'
 };
