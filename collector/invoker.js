1dd5479ba7faf5f65eaa9a6a5c5125df38b66d8b
Enrique Daimiel <enrique.daimiel@bbva.com>
2017-09-27 14:55:53 +0200

    refactor: catch unhandled promise rejection

--- collector/invoker.js
+++ collector/invoker.js
@@ -26,8 +26,8 @@ AWS.config.update({region:'eu-west-1'});
 
 function assumeAWSRole(accountId){
   var params = {
-    DurationSeconds: 900, 
-    RoleArn: "arn:aws:iam::"+accountId+":role/test_delegated_cloudwatch_metrics_role", 
+    DurationSeconds: 900,
+    RoleArn: "arn:aws:iam::"+accountId+":role/test_delegated_cloudwatch_metrics_role",
     RoleSessionName: "MirrorGate"
   };
   return sts.assumeRole(params).promise();
@@ -35,10 +35,10 @@ function assumeAWSRole(accountId){
 
 function metricsCallback(err, data){
   if (err) {
-    console.log(err, err.stack); 
+    console.log(err, err.stack);
   } else {
     console.log(data);
-  }    
+  }
 }
 
 function addDimensions(template, metricName, loadBalancer){
@@ -50,7 +50,7 @@ function addDimensions(template, metricName, loadBalancer){
 
   AWSBalancer.MetricName = metricName;
   console.log(AWSBalancer);
-  
+
   return AWSBalancer;
 }
 
@@ -78,26 +78,30 @@ function cloudWatchInvoker(){
       var accountId = cleanALB.substring(0,splitLocation);
       var albName = cleanALB.substring(splitLocation+1, cleanALB.length);
 
-      assumeAWSRole(accountId).then((element) => {
-        cloudWatch = new AWS.CloudWatch({
-          credentials: {
-            accessKeyId: element.Credentials.AccessKeyId,
-            secretAccessKey: element.Credentials.SecretAccessKey,
-            sessionToken: element.Credentials.SessionToken,
-          }
-        });
-        Promise.all(createInput(albName))
-        .then(results => {
-          APICaller.sendResultsToMirrorgate(results, cleanALB)
-            .then(result => {
-                console.log("Elements sent to Mirrorgate");
-                console.log(result); })
-            .catch(error => {
-                console.log("POST to Mirrorgate failed!");
-                console.log(error);
-            });
-        }).catch(error => console.log(error));
-      });
+      assumeAWSRole(accountId)
+        .then((element) => {
+          cloudWatch = new AWS.CloudWatch({
+            credentials: {
+              accessKeyId: element.Credentials.AccessKeyId,
+              secretAccessKey: element.Credentials.SecretAccessKey,
+              sessionToken: element.Credentials.SessionToken,
+            }
+          });
+          Promise.all(createInput(albName))
+            .then(results => {
+              APICaller.sendResultsToMirrorgate(results, cleanALB)
+                .then(result => {
+                    console.log("Elements sent to Mirrorgate");
+                    console.log(result);
+                })
+                .catch(error => {
+                    console.log("POST to Mirrorgate failed!");
+                    console.log(error);
+                });
+            })
+            .catch(error => console.log(error));
+        })
+        .catch(error => console.log(error));
     });
   }).catch((error) => {
     console.log(error);
